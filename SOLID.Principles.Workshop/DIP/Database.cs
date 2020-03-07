4b893ff67f52e3884c6374e379bcb43086732453
Petri Miikki <petri.miikki@visma.com>
2020-03-07 18:46:07 +0200

    - Exercise for DIP refacotring

---
+++ SOLID.Principles.Workshop/DIP/Database.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace DIP
+{
+    public sealed class Database
+    {
+        public Task<T> Query<T>(string sql, object parameters)
+        {
+            return Task.FromResult(default(T));
+        }
+
+        public Task Execute(string SQL, object parameters)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SOLID.Principles.Workshop/DIP/HttpSession.cs
@@ -0,0 +1,20 @@
+﻿namespace DIP
+{
+    public sealed class HttpSession
+    {
+        public int UserId { get; }
+        public int CompanyId { get; }
+
+        public static HttpSession GetInstance()
+        {
+            return new HttpSession(0, 0);
+        }
+
+        private HttpSession(int userId, int companyId)
+        {
+            UserId = userId;
+            CompanyId = companyId;
+        }
+
+    }
+}
