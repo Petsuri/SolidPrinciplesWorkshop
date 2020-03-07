f979453b4f51fc27c1fb1cf6cb005c268ecb33d5
Petri Miikki <petri.miikki@visma.com>
2020-03-07 14:45:15 +0200

    - More logic for refactoring purposes to practice how to follow SRP

--- SOLID.Principles.Workshop/SRP/Pizzeria.cs
+++ SOLID.Principles.Workshop/SRP/Pizzeria.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SRP
 {
     public sealed class Pizzeria
     {
-        private static IReadOnlyDictionary<string, decimal> Toppings = new Dictionary<string, decimal>()
+        private static readonly IReadOnlyDictionary<string, decimal> Toppings = new Dictionary<string, decimal>()
         {
             {"Salami", 3.0m},
             {"Mozzarella Di Bufala", 2.5m},
@@ -17,7 +18,7 @@ namespace SRP
             {"Shrimp", 3.25m },
         };
 
-        private static IReadOnlyDictionary<string, decimal> Pizzas = new Dictionary<string, decimal>()
+        private static readonly IReadOnlyDictionary<string, decimal> Pizzas = new Dictionary<string, decimal>()
         {
             {"Margarita", 8.0m },
             {"Fantasia", 12.0m },
@@ -25,7 +26,7 @@ namespace SRP
         };
 
 
-        public void TakeAnOrder(string ordererEmail, string pizza, List<string> additionalToppings)
+        public async Task TakeAnOrder(string ordererEmail, string pizza, List<string> additionalToppings)
         {
             var isValidEmail = new EmailAddressAttribute().IsValid(ordererEmail);
             if (!isValidEmail)
@@ -39,8 +40,42 @@ namespace SRP
             }
 
             var isAllToppingsValid = additionalToppings.All(Toppings.ContainsKey);
+            if (!isAllToppingsValid)
+            {
+                var notFoundTopping = additionalToppings.First(x => !Toppings.ContainsKey(x));
+                throw new ToppingNotFoundException(notFoundTopping);
+            }
 
+            var pizzaPrice = Pizzas[pizza] + additionalToppings.Sum(x => Toppings[x]);
 
+            //Give discount of 5% if 5 additional toppings has been added, excluding Fantasia
+            if (5 <= additionalToppings.Count && pizza != "Fantasia")
+            {
+                pizzaPrice *= 0.95m;
+            }
+
+            var db = new Database();
+            var customerId = await db.Query<int?>("SELECT ID FROM Customers WHERE Email = @Email", new {Email = ordererEmail});
+
+            if (!customerId.HasValue)
+            {
+                await db.Execute("INSERT INTO Customers (Email) VALUES (@Email)", new {Email = ordererEmail});
+                customerId = await db.Query<int?>("SELECT ID FROM Customers WHERE Email = @Email",
+                    new {Email = ordererEmail});
+            }
+
+            await db.Execute(
+                "INSERT INTO Orders (OrdererEmail, Pizza, ListOfToppings, Price) Values (@Email, @Pizza, @ListOfToppings, @Price)",
+                new
+                {
+                    Email = ordererEmail,
+                    Pizza = pizza,
+                    ListOfToppings = string.Join(",", additionalToppings),
+                    Price = pizzaPrice
+                });
+
+            var email = new EmailSender();
+            await email.Send(ordererEmail, "Your order has been received");
         }
 
     }
