4aee8cc83c6bff554d6b29201c585138e6bfd7b3
Lukas Heyny <Lukas.Heyny@visma.com>
2021-10-29 14:24:51 +0300

    Template method applied

---
+++ SOLID.Principles.Workshop/OCP.UnitTests/Method/TestingPetrisPizzeriaMembership.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OCP.Method
+{
+    public class TestingPetrisPizzeriaMembership : PetrisPizzeriaMembership
+    {
+        private readonly DateTime _currentTime = new DateTime(2000, 1, 1);
+
+        public TestingPetrisPizzeriaMembership(Guid id, DateTime? expireTime, DateTime? currentTime = null) : base(id, expireTime)
+        {
+            _currentTime = currentTime ?? _currentTime;
+        }
+
+        protected override DateTime CurrentTime()
+        {
+            return _currentTime;
+        }
+    }
+}
